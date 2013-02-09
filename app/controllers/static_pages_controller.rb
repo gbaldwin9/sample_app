eb0d5f39c3a33d42f0ce43873199f113c33b2cfb
Gary Baldwin <gbaldwin9@gmail.com>
2013-02-09 09:43:31 -0600

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+  		 @title = "Home"
   end
 
   def help
   end
 
   def about 
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
 
-  get "static_pages/about"
+  get "users/new"
 
+root to: 'static_pages#home'
+
+
+
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help', :as => :help
+  match '/about',   to: 'static_pages#about', :as => :about
+  match '/contact', to: 'static_pages#contact', :as => :contact
+  match 'static_pages/home',    to: 'static_pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
